247fe97ebe158e00914a5c848ab0cc5e7a1e969b
satriapamudjii <163331188+satriapamudjii@users.noreply.github.com>
2024-08-04 13:15:41 +0200

    error catching update

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, exc
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 
@@ -6,11 +7,20 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=False)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db_connection():
-    conn = engine.connect()
+    db_session = SessionLocal()
     try:
-        yield conn
+        yield db_session
+    except exc.DBAPIError as e:
+        if e.connection_invalidated:
+            print("Database connection was invalidated!")
+        else:
+            print("Database error:", e)
+    except exc.SQLAlchemyError as e:
+        print("An error occurred with SQLAlchemy:", e)
     finally:
-        conn.close()
+        db_session.close()
