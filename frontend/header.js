7ab743337874820a77f2fe9a7f7464edd18765af
satriapamudjii <163331188+satriapamudjii@users.noreply.github.com>
2024-11-03 14:49:53 +0100

    Commit header.js

---
+++ frontend/header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" exact activeClassName="active">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" activeClassName="active">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/services" activeClassName="active">Services</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
